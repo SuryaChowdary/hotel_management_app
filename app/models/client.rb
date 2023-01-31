d5cc4ae27ad9e2171186f62393c4e27a83ec4618
surya <surya@singular.dev>
2023-01-31 11:39:36 +0530

    Added dependent destroy in necessary models

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,7 @@ class Client < ApplicationRecord
   has_many :regions, through: :client_regions
   has_many :client_locations
   has_many :locations, through: :client_locations
-  has_many :hotel_branches
+  has_many :hotel_branches, dependent: :destroy
   validates :name, presence: true
   belongs_to :user
   accepts_nested_attributes_for :hotel_branches
--- app/models/room_facility_category.rb
+++ app/models/room_facility_category.rb
@@ -1,5 +1,5 @@
 class RoomFacilityCategory < ApplicationRecord
-  has_many :room_facilities
+  has_many :room_facilities, dependent: :destroy
   has_many :rooms, through: :room_facilities
   validates :name, presence: true
 end
